f5e4ad0046a45f9317990b4ab151be10a33ac369
Auxxme <90456467+Auxxme@users.noreply.github.com>
2021-10-06 06:24:13 -0700

    Create accomodation.php

---
+++ accomodation.php
@@ -0,0 +1,338 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <link rel="icon" href="img/fav-icon.png" type="image/x-icon" />
+      
+        <title>HillTown Resort</title>
+
+        <!-- Icon css link -->
+        <link href="css/font-awesome.min.css" rel="stylesheet">
+        <link href="vendors/stroke-icon/style.css" rel="stylesheet">
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        
+        <!-- Rev slider css -->
+        <link href="vendors/revolution/css/settings.css" rel="stylesheet">
+        <link href="vendors/revolution/css/layers.css" rel="stylesheet">
+        <link href="vendors/revolution/css/navigation.css" rel="stylesheet">
+        <link href="vendors/animate-css/animate.css" rel="stylesheet">
+        
+        <!-- Extra plugin css -->
+        <link href="vendors/magnify-popup/magnific-popup.css" rel="stylesheet">
+        <link href="vendors/owl-carousel/owl.carousel.min.css" rel="stylesheet">
+        <link href="vendors/bootstrap-datepicker/bootstrap-datetimepicker.min.css" rel="stylesheet">
+        <link href="vendors/bootstrap-selector/bootstrap-select.css" rel="stylesheet">
+        <link href="vendors/lightbox/simpleLightbox.css" rel="stylesheet">
+        
+        <link href="css/style.css" rel="stylesheet">
+        <link href="css/responsive.css" rel="stylesheet">
+
+       
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+
+      <?php
+        include './header.php';
+        ?>
+        
+        <!--================Banner Area =================-->
+        <section class="banner_area">
+            <div class="container">
+                <div class="banner_inner_content">
+                    <h3>Rooms</h3>
+                    <ul>
+                        <li class="active"><a href="index.html">Home</a></li>
+                        <li><a href="room.html">Rooms</a></li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        <!--================End Banner Area =================-->
+        
+        <!--================Explor Room Area =================-->
+        <section class="explor_room_area explore_room_list">
+            <div class="container">
+                <div class="explor_title row m0">
+                    <div class="left_ex_title">
+                        <h2>Explor Our <span>rooms</span></h2>
+                        <p>choose room according to budget</p>
+                    </div>
+                </div>
+                <div class="row explor_room_item_inner">
+                    <div class="col-md-4 col-sm-6">
+                        <div class="explor_item">
+                            <a href="room-details.html" class="room_image">
+                                <img src="img/room/room-1.jpg" alt="">
+                            </a>
+                            <div class="explor_text">
+                                <a href="room-details.html"><h4>Budget room</h4></a>
+                                <ul>
+                                    <li><a href="#">10x15</a></li>
+                                    <li><a href="#">Mountain view</a></li>
+                                    <li><a href="#">2 Balcony</a></li>
+                                </ul>
+                                <div class="explor_footer">
+                                    <div class="pull-left">
+                                        <h3>$170 <span>/ Night</span></h3>
+                                    </div>
+                                    <div class="pull-right">
+                                        <a class="book_now_btn" href="room-details.html">View details</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6">
+                        <div class="explor_item">
+                            <a href="room-details.html" class="room_image">
+                                <img src="img/room/room-2.jpg" alt="">
+                            </a>
+                            <div class="explor_text">
+                                <a href="room-details.html"><h4>Budget room</h4></a>
+                                <ul>
+                                    <li><a href="#">10x15</a></li>
+                                    <li><a href="#">Mountain view</a></li>
+                                    <li><a href="#">2 Balcony</a></li>
+                                </ul>
+                                <div class="explor_footer">
+                                    <div class="pull-left">
+                                        <h3>$170 <span>/ Night</span></h3>
+                                    </div>
+                                    <div class="pull-right">
+                                        <a class="book_now_btn" href="room-details.html">View details</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6">
+                        <div class="explor_item">
+                            <a href="room-details.html" class="room_image">
+                                <img src="img/room/room-3.jpg" alt="">
+                            </a>
+                            <div class="explor_text">
+                                <a href="room-details.html"><h4>Budget room</h4></a>
+                                <ul>
+                                    <li><a href="#">10x15</a></li>
+                                    <li><a href="#">Mountain view</a></li>
+                                    <li><a href="#">2 Balcony</a></li>
+                                </ul>
+                                <div class="explor_footer">
+                                    <div class="pull-left">
+                                        <h3>$170 <span>/ Night</span></h3>
+                                    </div>
+                                    <div class="pull-right">
+                                        <a class="book_now_btn" href="room-details.html">View details</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6">
+                        <div class="explor_item">
+                            <a href="room-details.html" class="room_image">
+                                <img src="img/room/room-1.jpg" alt="">
+                            </a>
+                            <div class="explor_text">
+                                <a href="room-details.html"><h4>Budget room</h4></a>
+                                <ul>
+                                    <li><a href="#">10x15</a></li>
+                                    <li><a href="#">Mountain view</a></li>
+                                    <li><a href="#">2 Balcony</a></li>
+                                </ul>
+                                <div class="explor_footer">
+                                    <div class="pull-left">
+                                        <h3>$170 <span>/ Night</span></h3>
+                                    </div>
+                                    <div class="pull-right">
+                                        <a class="book_now_btn" href="room-details.html">View details</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6">
+                        <div class="explor_item">
+                            <a href="room-details.html" class="room_image">
+                                <img src="img/room/room-2.jpg" alt="">
+                            </a>
+                            <div class="explor_text">
+                                <a href="room-details.html"><h4>Budget room</h4></a>
+                                <ul>
+                                    <li><a href="#">10x15</a></li>
+                                    <li><a href="#">Mountain view</a></li>
+                                    <li><a href="#">2 Balcony</a></li>
+                                </ul>
+                                <div class="explor_footer">
+                                    <div class="pull-left">
+                                        <h3>$170 <span>/ Night</span></h3>
+                                    </div>
+                                    <div class="pull-right">
+                                        <a class="book_now_btn" href="room-details.html">View details</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6">
+                        <div class="explor_item">
+                            <a href="room-details.html" class="room_image">
+                                <img src="img/room/room-3.jpg" alt="">
+                            </a>
+                            <div class="explor_text">
+                                <a href="room-details.html"><h4>Budget room</h4></a>
+                                <ul>
+                                    <li><a href="#">10x15</a></li>
+                                    <li><a href="#">Mountain view</a></li>
+                                    <li><a href="#">2 Balcony</a></li>
+                                </ul>
+                                <div class="explor_footer">
+                                    <div class="pull-left">
+                                        <h3>$170 <span>/ Night</span></h3>
+                                    </div>
+                                    <div class="pull-right">
+                                        <a class="book_now_btn" href="room-details.html">View details</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6">
+                        <div class="explor_item">
+                            <a href="room-details.html" class="room_image">
+                                <img src="img/room/room-1.jpg" alt="">
+                            </a>
+                            <div class="explor_text">
+                                <a href="room-details.html"><h4>Budget room</h4></a>
+                                <ul>
+                                    <li><a href="#">10x15</a></li>
+                                    <li><a href="#">Mountain view</a></li>
+                                    <li><a href="#">2 Balcony</a></li>
+                                </ul>
+                                <div class="explor_footer">
+                                    <div class="pull-left">
+                                        <h3>$170 <span>/ Night</span></h3>
+                                    </div>
+                                    <div class="pull-right">
+                                        <a class="book_now_btn" href="room-details.html">View details</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6">
+                        <div class="explor_item">
+                            <a href="room-details.html" class="room_image">
+                                <img src="img/room/room-2.jpg" alt="">
+                            </a>
+                            <div class="explor_text">
+                                <a href="room-details.html"><h4>Budget room</h4></a>
+                                <ul>
+                                    <li><a href="#">10x15</a></li>
+                                    <li><a href="#">Mountain view</a></li>
+                                    <li><a href="#">2 Balcony</a></li>
+                                </ul>
+                                <div class="explor_footer">
+                                    <div class="pull-left">
+                                        <h3>$170 <span>/ Night</span></h3>
+                                    </div>
+                                    <div class="pull-right">
+                                        <a class="book_now_btn" href="room-details.html">View details</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6">
+                        <div class="explor_item">
+                            <a href="room-details.html" class="room_image">
+                                <img src="img/room/room-3.jpg" alt="">
+                            </a>
+                            <div class="explor_text">
+                                <a href="room-details.html"><h4>Budget room</h4></a>
+                                <ul>
+                                    <li><a href="#">10x15</a></li>
+                                    <li><a href="#">Mountain view</a></li>
+                                    <li><a href="#">2 Balcony</a></li>
+                                </ul>
+                                <div class="explor_footer">
+                                    <div class="pull-left">
+                                        <h3>$170 <span>/ Night</span></h3>
+                                    </div>
+                                    <div class="pull-right">
+                                        <a class="book_now_btn" href="room-details.html">View details</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <nav aria-label="Page navigation" class="room_pagination">
+                    <ul class="pagination">
+                        <li>
+                            <a href="#" aria-label="Previous">
+                                <i class="fa fa-angle-left" aria-hidden="true"></i>
+                            </a>
+                        </li>
+                        <li><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li>
+                            <a href="#" aria-label="Next">
+                                <i class="fa fa-angle-right" aria-hidden="true"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </section>
+        <!--================End Explor Room Area =================-->
+        
+      
+        <?php
+        include './footer.php';
+        ?>
+        
+        
+        
+        <!--================End Footer Area =================-->
+        
+        <script src="js/jquery-2.2.4.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+        <!-- Rev slider js -->
+        <script src="vendors/revolution/js/jquery.themepunch.tools.min.js"></script>
+        <script src="vendors/revolution/js/jquery.themepunch.revolution.min.js"></script>
+        <script src="vendors/revolution/js/extensions/revolution.extension.video.min.js"></script>
+        <script src="vendors/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+        <script src="vendors/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+        <script src="vendors/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+        
+        <script src="vendors/magnify-popup/jquery.magnific-popup.min.js"></script>
+        <script src="vendors/isotope/imagesloaded.pkgd.min.js"></script>
+        <script src="vendors/isotope/isotope.pkgd.min.js"></script>
+        <script src="vendors/counterup/waypoints.min.js"></script>
+        <script src="vendors/counterup/jquery.counterup.min.js"></script>
+        <script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+        <script src="vendors/bootstrap-datepicker/bootstrap-datetimepicker.min.js"></script>
+        <script src="vendors/bootstrap-selector/bootstrap-select.js"></script>
+        <script src="vendors/lightbox/simpleLightbox.min.js"></script>
+        
+        
+        <!-- instafeed-->
+        <script type="text/javascript" src="vendors/instafeed/instafeed.min.js"></script>
+        <script type="text/javascript" src="vendors/instafeed/script.js"></script>
+        
+        <script src="js/theme.js"></script>
+    </body>
+
+
+</html>
