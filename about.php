63f962f8ce7eb3814deea482496d07ff7e067d5e
Auxxme <90456467+Auxxme@users.noreply.github.com>
2021-10-06 06:22:22 -0700

    Create about.php

---
+++ about.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+
+<head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <link rel="icon" href="img/fav-icon.png" type="image/x-icon" />
+      
+        <title>HillTown Resort</title>
+
+        <!-- Icon css link -->
+        <link href="css/font-awesome.min.css" rel="stylesheet">
+        <link href="vendors/stroke-icon/style.css" rel="stylesheet">
+        <link href="vendors/flat-icon/flaticon.css" rel="stylesheet">
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        
+        <!-- Rev slider css -->
+        <link href="vendors/revolution/css/settings.css" rel="stylesheet">
+        <link href="vendors/revolution/css/layers.css" rel="stylesheet">
+        <link href="vendors/revolution/css/navigation.css" rel="stylesheet">
+        <link href="vendors/animate-css/animate.css" rel="stylesheet">
+        
+        <!-- Extra plugin css -->
+        <link href="vendors/magnify-popup/magnific-popup.css" rel="stylesheet">
+        <link href="vendors/owl-carousel/owl.carousel.min.css" rel="stylesheet">
+        <link href="vendors/bootstrap-datepicker/bootstrap-datetimepicker.min.css" rel="stylesheet">
+        <link href="vendors/bootstrap-selector/bootstrap-select.css" rel="stylesheet">
+        
+        <link href="css/style.css" rel="stylesheet">
+        <link href="css/responsive.css" rel="stylesheet">
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+
+      <?php
+        include './header.php';
+        ?> 
+        
+        <!--================Banner Area =================-->
+        <section class="banner_area">
+            <div class="container">
+                <div class="banner_inner_content">
+                    <h3>About Us</h3>
+                    <ul>
+                        <li class="active"><a href="index.html">Home</a></li>
+                        <li><a href="about-us.html">Activities</a></li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        <!--================End Banner Area =================-->
+        
+        <!--================Resort Story Area =================-->
+        <section class="introduction_area resort_story_area">
+            <div class="container">
+                <div class="row introduction_inner">
+                    <div class="col-md-5">
+                        <a href="#" class="introduction_img">
+                            <img src="img/resort-story-img.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="col-md-7">
+                        <div class="introduction_left_text">
+                            <div class="resort_title">
+                                <h2>our resort <span>story</span></h2>
+                                <h5>give best service to our customers</h5>
+                            </div>
+                            <br></br>
+                            <h4>We are Available for business</h4>
+                            <p>Hiltown resort colombo golf Resort & spa,Sri Lanka is an elegent,exclusive retreat on the beautiful western coast of srilanka,otherwise known as the pearl of the indian ocean.with a world-class golf course and signature CHI,The spa,the resort offers unrivalled facilities and hospitality.</p>
+                            <p>Set on the galle face promenade overlooking the Indian ocean,this upmarket hotel is surrounding by high-end shoppping and dining.It's a 6-minute walk from galle Face Green urban park and 2km from Beira Lake.</p>
+                            <p>Featuring city,lake or ocean views,the refined rooms offer free Wi-Fi,pluss minibars.upgraded rooms add balconies.suites offer seperate living rooms and access to a club lounge.</p>
+                            <p>There aew 4 resturant most with ocean views,as well as a whiskey and jazz bar,a lobby tea lounge,and a cocktail bar with aa terrace.Other amenities include an outdoor pool,a gym and posh spa.</p>
+                            <p>Check-in time:2.00 PM<br>Check-out time:12.00PM</br></p>
+                            <a class="about_btn_b" href="#">contact us</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--================End Resort Story Area =================-->
+        
+        <!--================Choose Resot Area =================-->
+        <section class="choose_resot_area">
+            <div class="container">
+                <div class="center_title">
+                    <h2>why choose our <span>resot</span></h2>
+                    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum</p>
+                </div>
+                <div class="row choose_resot_inner">
+                    <div class="col-md-5">
+                        <div class="resot_list">
+                            <ul>
+                                <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Wellness & poll</a></li>
+                                <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Free wifi</a></li>
+                                <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Bar & garden with terrace</a></li>
+                                <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Delicious breakfast</a></li>
+                                <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>HIgh customer satisfaction</a></li>
+                                <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Good parking & security</a></li>
+                                <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Clean room service</a></li>
+                                <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Discount coupons</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-7">
+                        <div class="choose_resot_slider owl-carousel">
+                            <div class="item">
+                                <img src="img/resot/resot-1.jpg" alt="">
+                            </div>
+                            <div class="item">
+                                <img src="img/resot/resot-1.jpg" alt="">
+                            </div>
+                            <div class="item">
+                                <img src="img/resot/resot-1.jpg" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--================End Choose Resot Area =================-->
+        
+        <!--================Client Testimonial Area =================-->
+        <section class="client_area client_three">
+            <div class="container">
+                <div class="clients_slider owl-carousel">
+                    <div class="item">
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="img/clients/client-1.png" alt="">
+                            </div>
+                            <div class="media-body">
+                                <p><i>“</i> Lovely hotel.It is good period staying this wonderful hotel.very comfortable.breakfast was really good..The suite was very good with comfortable furniture and the bed was comfortable too.we would definitely stay again. </p>
+                                <a href="#"><h4>- Heshin John</h4></a>
+                                <h5>Ceo of Minda</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="img/clients/client-1.png" alt="">
+                            </div>
+                            <div class="media-body">
+                                <p><i>“</i>Fantastic Holiday! The resort is very relaxed and it was a great place for the whole famoly to stay.There were plenty of activities for the kids and adults alike as well as simply relaxing and enjoying our holiday.The whole team are very freindly and nothing is too much trouble.Would definitely recommend this hotel to freiends and family.</p>
+                                <a href="#"><h4>- Michale John</h4></a>
+                                <h5>Ceo of Minda</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="img/clients/client-1.png" alt="">
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--================End Client Testimonial Area =================-->
+        
+        <!--================Video Area =================-->
+        <section class="video_area">
+            <div class="container">
+                <div class="video_inner">
+                    <a class="popup-youtube" href="https://www.youtube.com/watch?v=48uPk1SA37Y"><i class="flaticon-play-button"></i></a>
+                    <h4>virtual Tour</h4>
+                    <h5>of Hill Town Resort</h5>
+                </div>
+            </div>
+        </section>
+        <!--================End Video Area =================-->
+        
+        <!--================Fun Fact Area =================-->
+        <section class="fun_fact_area about_fun_fact">
+            <div class="container">
+                <div class="row">
+                    <div class="fun_fact_box row m0">
+                        <div class="col-md-3 col-sm-6">
+                            <div class="media">
+                                <div class="media-left">
+                                    <h3 class="counter">712</h3>
+                                </div>
+                                <div class="media-body">
+                                    <h4>new <br /> friendships</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <div class="media">
+                                <div class="media-left">
+                                    <h3 class="counter">254</h3>
+                                </div>
+                                <div class="media-body">
+                                    <h4>International <br /> Guests</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <div class="media">
+                                <div class="media-left">
+                                    <h3 class="counter">430</h3>
+                                </div>
+                                <div class="media-body">
+                                    <h4>five stars <br /> rating</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <div class="media">
+                                <div class="media-left">
+                                    <h3 class="counter">782</h3>
+                                </div>
+                                <div class="media-body">
+                                    <h4>Served <br /> Breakfast</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--================End Fun Fact Area =================-->
+        
+        
+        
+         <?php
+        include './footer.php';
+        ?>
+        
+        
+        <!--================End Footer Area =================-->
+       
+        <script src="js/jquery-2.2.4.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+        <!-- Rev slider js -->
+        <script src="vendors/revolution/js/jquery.themepunch.tools.min.js"></script>
+        <script src="vendors/revolution/js/jquery.themepunch.revolution.min.js"></script>
+        <script src="vendors/revolution/js/extensions/revolution.extension.video.min.js"></script>
+        <script src="vendors/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+        <script src="vendors/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+        <script src="vendors/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+        
+        <script src="vendors/magnify-popup/jquery.magnific-popup.min.js"></script>
+        <script src="vendors/isotope/imagesloaded.pkgd.min.js"></script>
+        <script src="vendors/isotope/isotope.pkgd.min.js"></script>
+        <script src="vendors/counterup/waypoints.min.js"></script>
+        <script src="vendors/counterup/jquery.counterup.min.js"></script>
+        <script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+        <script src="vendors/bootstrap-datepicker/bootstrap-datetimepicker.min.js"></script>
+        <script src="vendors/bootstrap-selector/bootstrap-select.js"></script>
+        <script src="vendors/lightbox/simpleLightbox.min.js"></script>
+        
+        <!-- instafeed-->
+        <script type="text/javascript" src="vendors/instafeed/instafeed.min.js"></script>
+        <script type="text/javascript" src="vendors/instafeed/script.js"></script>
+        
+        <script src="js/theme.js"></script>
+    </body>
+
+</html>
