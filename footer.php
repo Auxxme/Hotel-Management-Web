7a5475002bfe3c05d2ea77885393e28786675e89
Auxxme <90456467+Auxxme@users.noreply.github.com>
2021-10-06 06:25:34 -0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,97 @@
+ <!--================Footer Area =================-->
+        <footer class="footer_area">
+            <div class="footer_widget_area">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-3 col-xs-6">
+                            <aside class="f_widget about_widget">
+                                <img src="img/footer-logo.png" alt="">
+                                <div class="ab_wd_list">
+                                    <div class="media">
+                                        <div class="media-left">
+                                            <i class="fa fa-map-marker"></i>
+                                        </div>
+                                        <div class="media-body">
+                                            <h4>215/B Mallin Street,Colombo,Sri Lanka </h4>
+                                        </div>
+                                    </div>
+                                    <div class="media">
+                                        <div class="media-left">
+                                            <i class="fa fa-phone"></i>
+                                        </div>
+                                        <div class="media-body">
+                                            <h4>011 5486 412</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="book_now_area">
+                                    <a class="book_now_btn" href="#">Book now</a>
+                                </div>
+                            </aside>
+                        </div>
+                        <div class="col-md-3 col-xs-6">
+                            <aside class="f_widget link_widget">
+                                <div class="f_title">
+                                    <h3>Extra Links</h3>
+                                </div>
+                                <ul>
+                                    <li><a href="#">-  About Us</a></li>
+                                    <li><a href="#">-  Faq’s</a></li>
+                                    <li><a href="#">-  Blog</a></li>
+                                    <li><a href="#">-  Testimonials</a></li>
+                                    <li><a href="#">-  Reservation Now</a></li>
+                                </ul>
+                            </aside>
+                        </div>
+                        <div class="col-md-3 col-xs-6">
+                            <aside class="f_widget link_widget">
+                                <div class="f_title">
+                                    <h3>our services</h3>
+                                </div>
+                                <ul>
+                                    <li><a href="#">-  Food & Drinks</a></li>
+                                    <li><a href="#">-  Rooms</a></li>
+                                    <li><a href="#">-  Amenities</a></li>
+                                    <li><a href="#">-  Spa & Gym</a></li>
+                                    <li><a href="#">-  Hill Tours</a></li>
+                                </ul>
+                            </aside>
+                        </div>
+                        <div class="col-md-3 col-xs-6">
+                            <aside class="f_widget instagram_widget">
+                                <div class="f_title">
+                                    <h3>Instagram</h3>
+                                </div>
+                                <ul class="instagram_list" id="instafeed"></ul>
+                            </aside>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer_copyright_area">
+                <div class="container">
+                    <div class="pull-left">
+                        <h4>Copyright © HillTown Resort  <script>document.write(new Date().getFullYear());</script>. All rights reserved. </h4>
+                    </div>
+                    <div class="pull-right">
+                        <h4>Created by: <a href="#">DesignArc</a></h4>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!--================End Footer Area =================-->
+        
+        <!--================Search Box Area =================-->
+        <div class="search_area zoom-anim-dialog mfp-hide" id="test-search">
+            <div class="search_box_inner">
+                <h3>Search</h3>
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search for...">
+                    <span class="input-group-btn">
+                        <button class="btn btn-default" type="button"><i class="icon icon-Search"></i></button>
+                    </span>
+                </div>
+            </div>
+        </div>
+        <!--================End Search Box Area =================-->
+        
