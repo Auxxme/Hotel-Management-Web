95b63540164398b4cd64ef14c11b6834d19a4209
Auxxme <90456467+Auxxme@users.noreply.github.com>
2021-10-06 06:26:04 -0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,61 @@
+
+        <!--================Header Area =================-->
+        <header class="main_header_area">
+            <div class="header_top">
+                <div class="container">
+                    <div class="header_top_inner">
+                        <div class="pull-left">
+                            <a href="#"><i class="fa fa-phone"></i>+ (1800) 456 7890</a>
+                            <a href="#"><i class="fa fa-envelope-o"></i>info@hilltownresort.com</a>
+                        </div>
+                        <div class="pull-right">
+                            <ul class="header_social">
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="header_menu">
+                <nav class="navbar navbar-default">
+                    <div class="container">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <a class="navbar-brand" href="#">
+                                <img src="img/logo.png" alt="">
+                                <img src="img/logo-sticky.png" alt="">
+                            </a>
+                        </div>
+
+                        <!-- Collect the nav links, forms, and other content for toggling -->
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul class="nav navbar-nav">
+                                
+                                <li class="active"><a href="./">Home</a></li>
+                                <li><a href="about.php">About Us</a></li>
+                                <li><a href="contact.php">Contact Us</a></li>
+                                <li><a href="accomodation.php">Accomodation Us</a></li>
+                            </ul>
+                            <ul class="nav navbar-nav navbar-right">
+                                <li class="search_dropdown">
+                                    <a class="popup-with-zoom-anim" href="#test-search"><i class="icon icon-Search"></i></a>
+                                </li>
+                                <li class="book_btn">
+                                    <a class="book_now_btn" href="booking.php">Book now</a>
+                                </li>
+                            </ul>
+                        </div><!-- /.navbar-collapse -->
+                    </div><!-- /.container-fluid -->
+                </nav>
+            </div>
+        </header>
+        <!--================Header Area =================-->
